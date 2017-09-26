ddd8fee5eb49bb6789c64125e8520a5615990452
minhtranzentech <leap.ahead.tran@gmail.com>
2017-09-26 09:02:54 -0600

    no message

---
+++ Ass-1.playground/Pages/Overview.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: # Assignment 1
+//: ## Swift Programming: First Steps
+//:
+//: This is the first assignment for CPSC 599.72 ("iProgramming for Creative Minds"), Fall 2017
+
+//: ## Instructions
+//:
+//: This assignment is a Swift Playground document. It consists of three (3) pages. Go through each page, read the task descriptions and add your Swift code accordingly.
+
+//: ## Submission
+//:
+//: This assignment playground needs to be submitted through our D2L course website. See our course website for details about the submission date and time. 
+//:
+//: Should you be late with your submission, then you will incur a **50% penalty** on the points of this assignment.
+
+
+//: [Let's get started ...](@next)
